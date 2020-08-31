33f42de0169a264e92cf96bddd8fa014f5b96f64
haoliu <liuhao01@inke.cn>
2020-08-31 22:15:55 +0800

    support save user own tag template #14

--- src/main/kotlin/Runner.kt
+++ src/main/kotlin/Runner.kt
@@ -1,10 +1,6 @@
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.wm.WindowManager
 import entry.Builder
-import java.awt.Dimension
-import java.util.*
-import javax.swing.JFrame
 
 internal class Runner {
     fun run(e: AnActionEvent, builder: Builder) {
@@ -12,19 +8,23 @@ internal class Runner {
             e.getData(PlatformDataKeys.PROJECT)
         val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
         val selectedText = editor.selectionModel.selectedText
-        val tf = TextCopyForm()
-        val frame = TextCopyForm.getFrame()
-        tf.setBuilder(builder);
-        val p = Objects.requireNonNull(WindowManager.getInstance().getFrame(project))
-            ?.locationOnScreen
-        frame.location = p
-        tf.t1TextArea.text = selectedText
+        val tf = TextCopyForm(project)
+//        val framee = TextCopyForm.getFrame()
+        tf.setBuilder(builder,selectedText)
+//        val p = Objects.requireNonNull(WindowManager.getInstance().getFrame(project))
+//            ?.locationOnScreen
+//        frame.location = project
+//        tf.t1TextArea.text = selectedText
         tf.gen()
 
-        frame.contentPane = tf.panel1
-        frame.preferredSize = Dimension(1100, 600)
-        frame.extendedState = JFrame.NORMAL
-        frame.pack()
-        frame.isVisible = true
+        tf.pack()
+        tf.show()
+      //  tf.setBuilder(builder)
+
+       // frame.contentPane = tf.panel1
+        //frame.preferredSize = Dimension(1100, 600)
+        //frame.extendedState = JFrame.NORMAL
+        //frame.pack()
+        //frame.isVisible = true
     }
 }
