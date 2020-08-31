2379a8e36b118a053fe11da4054a92d5b5eec5c5
haoliu <liuhao01@inke.cn>
2020-08-31 22:20:19 +0800

    remove unused comments

--- src/main/kotlin/Runner.kt
+++ src/main/kotlin/Runner.kt
@@ -9,22 +9,9 @@ internal class Runner {
         val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
         val selectedText = editor.selectionModel.selectedText
         val tf = TextCopyForm(project)
-//        val framee = TextCopyForm.getFrame()
         tf.setBuilder(builder,selectedText)
-//        val p = Objects.requireNonNull(WindowManager.getInstance().getFrame(project))
-//            ?.locationOnScreen
-//        frame.location = project
-//        tf.t1TextArea.text = selectedText
         tf.gen()
-
         tf.pack()
         tf.show()
-      //  tf.setBuilder(builder)
-
-       // frame.contentPane = tf.panel1
-        //frame.preferredSize = Dimension(1100, 600)
-        //frame.extendedState = JFrame.NORMAL
-        //frame.pack()
-        //frame.isVisible = true
     }
 }
